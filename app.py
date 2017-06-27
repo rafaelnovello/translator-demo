097b6f281f027e55a674c1bcf1f506033efa64b8
Rafael Novello <rafa.reis.novello@gmail.com>
2017-06-27 19:03:33 -0300

    Adding TinyDB and vocab page

--- app.py
+++ app.py
@@ -2,12 +2,10 @@
 
 import helper
 
-import tensorflow as tf
 from japronto import Application
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-_, (source_vocab_to_int, _), (_, target_int_to_vocab) = helper.load_preprocess()
 env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html'])
@@ -17,7 +15,8 @@ env = Environment(
 def index(request):
     if request.method == 'POST':
         source = request.form.get('source', '')
-        translated = translate(source)
+        translated = helper.translate(source)
+        helper.save_translation(source, translated)
     else:
         translated = ''
 
@@ -28,41 +27,18 @@ def index(request):
     )
 
 
-def sentence_to_seq(sentence, vocab_to_int):
-    sentence = sentence.lower()
-    unk_id = vocab_to_int.get('<UNK>')
-    ids = [vocab_to_int.get(word, unk_id) for word in sentence.split()]
-    return ids
+def words(request):
+    with open('words.txt', 'r') as f:
+        words = f.readlines()
 
+    template = env.get_template('vocab.html')
+    return request.Response(
+        text=template.render(words=words),
+        mime_type='text/html'
+    )
 
-def translate(translate_sentence):
-    batch_size = 256
-    load_path = 'checkpoint/dev'
-    translate_sentence = sentence_to_seq(translate_sentence, source_vocab_to_int)
-
-    loaded_graph = tf.Graph()
-    with tf.Session(graph=loaded_graph) as sess:
-        # Load saved model
-        loader = tf.train.import_meta_graph(load_path + '.meta')
-        loader.restore(sess, load_path)
-
-        input_data = loaded_graph.get_tensor_by_name('input:0')
-        logits = loaded_graph.get_tensor_by_name('predictions:0')
-        target_sequence_length = loaded_graph.get_tensor_by_name('target_sequence_length:0')
-        source_sequence_length = loaded_graph.get_tensor_by_name('source_sequence_length:0')
-        keep_prob = loaded_graph.get_tensor_by_name('keep_prob:0')
-
-        translate_logits = sess.run(
-            logits, {
-                input_data: [translate_sentence]*batch_size,
-                target_sequence_length: [len(translate_sentence)*2]*batch_size,
-                source_sequence_length: [len(translate_sentence)]*batch_size,
-                keep_prob: 1.0
-            }
-        )[0]
-        translated = " ".join([target_int_to_vocab[i] for i in translate_logits[:-1]])
-        return translated
 
 app = Application()
 app.router.add_route('/', index, methods=['GET', 'POST'])
+app.router.add_route('/vocab/', words, methods=['GET'])
 app.run(debug=True)
